bbd6260995878fe81721316d28dea8918bf8e8a6
nebrassy <nebras30@gmail.com>
2019-09-16 02:39:45 +0300

    initial import

---
+++ twrp.mk
@@ -0,0 +1,4 @@
+TWRP_PATH := device/twrp/enchilada
+
+TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
+TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip
