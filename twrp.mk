3011718a9da29ad11e0fdcafa1c28034e19f8b9f
nebrassy <nebras30@gmail.com>
2019-11-20 00:03:02 +0200

    remove ramdisk-recovery.img
    * no longer needed
    
    Change-Id: I187557aea26df34637e2da8ca47e2863bcfa8147

--- twrp.mk
+++ twrp.mk
@@ -1,4 +1,3 @@
 TWRP_PATH := device/twrp/enchilada
 
-TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
 TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip
